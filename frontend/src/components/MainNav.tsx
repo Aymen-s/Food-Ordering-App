6ae9920e93dcdffe23fb6d5c233b691e99dc3168
Aymen <aymensileshi77@gmail.com>
2025-04-21 22:17:59 +0300

    completing the main nav

---
+++ frontend/src/components/MainNav.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./ui/button";
+
+const MainNav = () => {
+  return (
+    <Button
+      variant="ghost"
+      className="font-bold hover:text-orange-500 hover:bg-white cursor-pointer"
+    >
+      Log In
+    </Button>
+  );
+};
+
+export default MainNav;
