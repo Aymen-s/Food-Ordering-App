c114f21ea1c0f764c2c3996764505fdbc3d59c70
Aymen <aymensileshi77@gmail.com>
2025-04-23 17:03:02 +0300

    creating a user request on the frontend

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,14 +1,12 @@
+import { useCreateMyUser } from "@/api/MyUserApi";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
 
 type Props = {
   children: React.ReactNode;
 };
 
-const onRedirectCallback = (appState?: AppState, user?: User) => {
-  console.log("USER", user);
-};
-
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
+  const { createUser } = useCreateMyUser();
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URI;
@@ -16,6 +14,15 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
   if (!domain || !clientId || !redirectUri) {
     throw new Error("Unable to initialise auth");
   }
+
+  const onRedirectCallback = (appState?: AppState, user?: User) => {
+    if (user?.sub && user?.email) {
+      createUser({
+        auth0Id: user.sub,
+        email: user.email,
+      });
+    }
+  };
 
   return (
     <Auth0Provider
