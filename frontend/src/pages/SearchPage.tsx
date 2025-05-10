766e2522082a8d3c1453b58480881dfcd6e14fc8
Aymen <aymensileshi77@gmail.com>
2025-05-10 16:43:33 +0300

    sending search API request

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,8 +1,21 @@
+import { useSearchRestaurants } from "@/api/RestaurantApi";
 import { useParams } from "react-router-dom";
 
 const SearchPage = () => {
   const { city } = useParams();
-  return <span>user serach for {city}</span>;
+  const { results, isPending } = useSearchRestaurants(city);
+  return (
+    <span>
+      user serach for {city}
+      <span>
+        {results?.data.map((restaurant) => (
+          <span>
+            found - {restaurant.restaurantName}, {restaurant.city}
+          </span>
+        ))}
+      </span>
+    </span>
+  );
 };
 
 export default SearchPage;
