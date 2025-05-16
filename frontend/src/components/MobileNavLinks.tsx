e096d9123422ecaeb27acd442625ad8a472c125e
Aymen <aymensileshi77@gmail.com>
2025-05-16 18:28:23 +0300

    completing order status page

--- frontend/src/components/MobileNavLinks.tsx
+++ frontend/src/components/MobileNavLinks.tsx
@@ -7,6 +7,18 @@ const MobileNavLinks = () => {
   return (
     <>
       <Link
+        to="/order-status"
+        className="felx bg-white items-center font-bold hover:text-orange-500"
+      >
+        Order Status
+      </Link>
+      <Link
+        to="/manage-restaurant"
+        className="felx bg-white items-center font-bold hover:text-orange-500"
+      >
+        My Restaurant
+      </Link>
+      <Link
         to="/user-profile"
         className="felx bg-white items-center font-bold hover:text-orange-500"
       >
